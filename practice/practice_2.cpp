4da96411a12b2809479f100a5b22d5a6d3e65f45
emon2021 <mdemonahmed2021@gmail.com>
2024-10-09 18:52:54 +0600

    inverted triangle pyramid done

--- practice/practice_2.cpp
+++ practice/practice_2.cpp
@@ -9,14 +9,14 @@ int main()
 
     for(int row = 1; row <= n; row++)
     {
-        int space = n - row;
+        int space = 1;
 
-        for(space; space > 0; space--)
+        for(space; space < row; space++)
         {
             cout << " " ;
         }
 
-        for(int col = 1; col <= row; col++)
+        for(int col = n; col >= row; col--)
         {
             cout << "* "; // just use space after the stars symbol then it will create a triangle pyramid
         }
